65b87b2856e25df5fe4d1e0bbdb4ec6755535dce
ericksonlargura <erickson.largura@gmail.com>
2018-01-08 14:40:43 -0200

    ParallaxHome
    Base da home pronta, agora finalizando os detalhes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
+import { ParallaxDirective } from '../directives/parallax/parallax'
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    LoginPage
+    LoginPage,
+    ParallaxDirective
   ],
   imports: [
     BrowserModule,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  nome:string;
-  
-  constructor(public navCtrl: NavController, public navParams:NavParams) {
-    this.nome = this.navParams.get('nome');
-    console.log(this.nome);
-    
-  }
 
+  constructor(public navCtrl: NavController) {
+
+  }
 }
