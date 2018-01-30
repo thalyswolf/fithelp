7d96a8cca16a4eb3d9cb98f086cc73a084b31e78
ericksonlargura <erickson.largura@gmail.com>
2018-01-30 16:39:17 -0200

    Trabalhando nas Notificações

--- src/pages/notific/notific.ts
+++ src/pages/notific/notific.ts
@@ -15,14 +15,32 @@ import { HomePage } from '../home/home';
   templateUrl: 'notific.html',
 })
 export class NotificPage {
-
+  title:string="";
+  ver:boolean=true;
+  notifications:any = [{
+    title:"teste",
+    day:"seg",
+    hour:"12:20",
+    visible:false
+  }];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  showCont(v:boolean) {
+    this.ver = !this.ver;
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NotificPage');
   }
-
+  newNotif(){
+    this.notifications.push({
+      title:"ericksao",
+      day:"seg",
+      hour:"12:20",
+      visible:false
+    })
+  }
   onHome():void{
     this.navCtrl.push(HomePage,{
     });
